97f748c338833ed2a20497dd0ece4d296b44cd2b
wonju5332 <wonju5332@hanmail.net>
2020-04-02 21:50:50 +0900

    WJCoordinable 프로토콜 구현 WJAppdelegateCoordinator 클래스 구현

--- Example/WJCoordinator/AppDelegate.swift
+++ Example/WJCoordinator/AppDelegate.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import WJCoordinator
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coordinator:WJAppdelegateCoordinator?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        coordinator = WJAppdelegateCoordinator(window: window)
+        self.window = window
+        
+        coordinator?.start()
+        
         return true
     }
 
---
+++ WJCoordinator/Classes/WJCoordinator.swift
@@ -0,0 +1,29 @@
+
+public protocol WJCoordinable {
+    var window:UIWindow {get set}
+    var rootViewController:UINavigationController {get set}
+    func start()
+}
+
+open class WJAppdelegateCoordinator: WJCoordinable {
+    public var window: UIWindow
+    
+    public var rootViewController: UINavigationController
+    
+    public init(window:UIWindow) {
+        self.window = window
+        self.rootViewController = UINavigationController()
+        
+        let emptyViewController = UIViewController()
+        emptyViewController.view.backgroundColor = .cyan
+        rootViewController.pushViewController(emptyViewController, animated: false)
+    }
+    public func start() {
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        
+    }
+
+    
+}
+
